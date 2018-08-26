34acb0d66e767b12df587b0b3d3d4baec650c8ab
Matt Maree <matt.maree@tracker.co.za>
2018-08-26 16:56:14 +0200

    Project Creation

---
+++ src/main/java/com/example/rest/Login.java
@@ -0,0 +1,10 @@
+package com.example.rest;
+
+import javax.swing.*;
+
+public class Login {
+    private JTextField txtUsername;
+    private JTextField txtPassword;
+    private JButton btnLogin;
+
+}
